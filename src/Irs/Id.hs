5a32e1d1dd40c281262276f5e247d6abd1b728ed
Gabriel Torello <gabriel128@gmail.com>
2022-08-10 20:59:57 +1000

    Handle basic X86Var

--- src/Irs/Id.hs
+++ src/Irs/Id.hs
@@ -6,7 +6,16 @@ import Utils
 removeComplexExp :: Expr -> [(Stmt, Expr)]
 removeComplexExp (Const _) = []
 removeComplexExp (Var _) = []
-removeComplexExp expr = let varName = Utils.randVarName in [(Let varName expr, Var varName)]
+removeComplexExp expr =
+  let varName = Utils.randVarName
+   in [(Let varName expr, Var varName)]
+
+-- TODO: Handle the recursive casees
+-- let x = (1 + (2 + (3 + 4)))
+-- =>
+-- let y = (2 + (3 + 4)); let x = 1 + y
+-- =>
+-- let z = 3 + 4; let y = 2 + z; let x = 1 + y
 
 removeComplexStmt :: Stmt -> [Stmt]
 removeComplexStmt initStmt@(Let binding expr) = go (removeComplexExp expr)
