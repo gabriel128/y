bcb8cd4fc6730c56d66932bb884e84f7eafda5f4
Gabriel Torello <gabriel128@gmail.com>
2022-08-17 20:10:58 +1000

    Generate basic nasm x86 template with instructions

--- src/Irs/X86.hs
+++ src/Irs/X86.hs
@@ -1,46 +1,10 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use newtype instead of data" #-}
+
+-- | Represents type checked X86 in NASM format
 module Irs.X86 where
 
-import qualified Data.Text as T
+import qualified Irs.Nasm as Nasm
 
--- reg ::= rsp | rbp | rax | rbx | rcx | rdx | rsi | rdi | r8 | r9 | r10 | r11 | r12 | r13 | r14 | r15
--- arg ::=  $int | %reg | int(%reg)
--- instr ::= addq  arg, arg | subq  arg, arg | negq  arg | movq  arg, arg | callq label | pushq arg | popq arg | retq
--- prog ::=  .globl main
---         main:  instr^{+}
-
-type Label = T.Text
-
-data Arg = Imm Int | Reg | RegOffset Reg Int deriving (Eq, Show)
-
-data Instr
-  = Add Arg Arg
-  | Sub Arg Arg
-  | Neg Arg
-  | Mov Arg Arg
-  | Call Label
-  | Push Arg
-  | Pop Arg
-  | Ret
-  | Jmp Label
-  deriving (Eq, Show)
-
-data Reg
-  = Rsp
-  | Rbp
-  | Rax
-  | Rbx
-  | Rcx
-  | Rdx
-  | Rsi
-  | Rdi
-  | R8
-  | R9
-  | R10
-  | R11
-  | R12
-  | R13
-  | R14
-  | R15
-  deriving (Eq, Show)
-
-newtype X86Program = X86Program [Instr]
+newtype X86Program = X86Program [Nasm.Instr]
