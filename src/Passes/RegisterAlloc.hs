848a92f46a07126ee2c33ea8029d28be2cf93ecc
Gabriel Torello <gabriel128@gmail.com>
2023-09-04 20:34:48 +1000

    Fix livness algorithm

--- src/Passes/RegisterAlloc.hs
+++ src/Passes/RegisterAlloc.hs
@@ -7,13 +7,45 @@ import Data.Set (Set, difference, empty, fromList, union)
 import Data.Text (Text)
 import qualified Passes.PassEffs as PassEffs
 
--- Available Registers  rbx rcx rdx rsi rdi r8 r9 r10 r11 r12 r13 r14
+{-
+  # Register Allocation
 
-type LivenessBefore = Set Text
+  The register allocator will build liveness analysis and an interference graph to
+  find which variables can use a register insted of stack var mapping.
+
+  ## Liveness Analysis
+
+  The liveness analysis is processed backwards with the following equations
+
+  L(k) = (L(k+1) - W(k)) U R(k)
+
+  e.g. with x86 var pseudocode
+
+    movq $1, v        {}
+    movq $42, w       {v}
+    movq v, x         {v,w}
+    addq $7, x        {w,x}
+    movq x, y         {w,x}
+    movq x, z         {w,x,y}
+    addq w, z         {w,y,z}
+    movq y, t         {y,z}
+    negq t            {t,z}
+    movq z, %rax      {t,z}
+    addq t, %rax      {t}
+    jmp conclusion    {}
+-}
+
+-- Caller-save registers (the ones that a procedure can use freely):  rax rdx rcx rsi rdi r8 r9 r10 r11
+-- Callee-save registers (the ones that a procedure has to save and restore if used):
+--   rsp rbp rbx r12 r13 r14 r15
+
+-- Available Registers (rax is used as a tmp register) rbx rcx rdx rsi rdi r8 r9 r10 r11 r12 r13 r14
+
+type LivenessK = Set Text
 
 type WritesK = Set Text
 
-type LivenessAfterK = Set Text
+type LivenessKplus1 = Set Text
 
 type ReadsK = Set Text
 
@@ -28,18 +60,20 @@ buildLiveness :: [Stmt] -> [EnrichedStmt]
 buildLiveness stmts = snd $ foldr reducer (empty, []) stmts
   where
     reducer :: Stmt -> (Set Text, [EnrichedStmt]) -> (Set Text, [EnrichedStmt])
-    reducer stmt (livenessAfter, enrichedStmts) =
-      let liveness = buildStmtLiveness stmt livenessAfter
-          enrichedStmt = EnrichedStmt (StmtMetadata livenessAfter) stmt
+    reducer stmt (livenessKplus1, enrichedStmts) =
+      let liveness = buildStmtLiveness stmt livenessKplus1
+          enrichedStmt = EnrichedStmt (StmtMetadata liveness) stmt
        in (liveness, enrichedStmt : enrichedStmts)
 
-buildStmtLiveness :: Stmt -> LivenessAfterK -> LivenessBefore
-buildStmtLiveness (Let binding expr) livenessAfter =
-  livenessBeforeK livenessAfter (fromList [binding]) (readsFromExpr expr)
-buildStmtLiveness stmt@(Return expr) livenessAfter =
-  livenessBeforeK livenessAfter empty (readsFromExpr expr)
-buildStmtLiveness stmt@(Print expr) livenessAfter =
-  livenessBeforeK livenessAfter empty (readsFromExpr expr)
+-- Insights:
+-- in a let expression
+buildStmtLiveness :: Stmt -> LivenessKplus1 -> LivenessK
+buildStmtLiveness (Let binding expr) livenessKplus1 =
+  livenessK livenessKplus1 (fromList [binding]) (readsFromExpr expr)
+buildStmtLiveness stmt@(Return expr) livenessKplus1 =
+  livenessK livenessKplus1 empty (readsFromExpr expr)
+buildStmtLiveness stmt@(Print expr) livenessKplus1 =
+  livenessK livenessKplus1 empty (readsFromExpr expr)
 
 readsFromExpr :: Expr -> Set Text
 readsFromExpr (Const _) = empty
@@ -48,7 +82,7 @@ readsFromExpr (Var binding) = fromList [binding]
 readsFromExpr (UnaryOp _ expr) = readsFromExpr expr
 readsFromExpr (BinOp _ expr expr') = readsFromExpr expr `union` readsFromExpr expr'
 
--- L_after(n) = {}
--- L_before(k)  = (L_after(k) - W(k)) \union R(k)
-livenessBeforeK :: LivenessAfterK -> WritesK -> ReadsK -> Set Text
-livenessBeforeK lafter writesK readsK = (lafter `difference` writesK) `union` readsK
+--  L(k) = (L(k+1) - W(k)) U R(k)
+-- where W(k) means writes for line k and R(k) means for reads for linke k
+livenessK :: LivenessKplus1 -> WritesK -> ReadsK -> Set Text
+livenessK livenessKplus1 writesK readsK = (livenessKplus1 `difference` writesK) `union` readsK
--- test/RegisterAllocTests.hs
+++ test/RegisterAllocTests.hs
@@ -2,6 +2,7 @@ module RegisterAllocTests (test_reg_alloc) where
 
 import Ast.Ast
 import Data.Set
+import qualified Data.Text as T
 import Passes.RegisterAlloc
 import Passes.RegisterAlloc (stmt)
 import Test.Tasty (TestTree, testGroup)
@@ -10,22 +11,41 @@ import Test.Tasty.HUnit (assertBool, assertEqual, testCase)
 test_reg_alloc :: TestTree
 test_reg_alloc = testGroup "Tests" [unitTests]
 
+--  L(k) = (L(k+1) - W(k)) U R(k)
 ex1 :: [Stmt]
 ex1 =
-  [ Let "a" (Const 5),
-    Let "b" (Const 30),
-    Let "c" (Var "a"),
-    Let "b" (Const 10),
-    Let "b" (BinOp Add (Var "b") (Var "c"))
+  [ Let "a" (Const 5), -- {}
+    Let "b" (Const 30), -- {a}
+    Let "c" (Var "a"), -- {a}
+    Let "b" (Const 10), -- {c}
+    Let "b" (BinOp Add (Var "b") (Var "c")) -- {b, c}
   ]
 
 enrichedEx1 :: [EnrichedStmt]
 enrichedEx1 =
-  [ EnrichedStmt (StmtMetadata (fromList ["a"])) (Let "a" (Const 5)),
+  [ EnrichedStmt (StmtMetadata (fromList [])) (Let "a" (Const 5)),
     EnrichedStmt (StmtMetadata (fromList ["a"])) (Let "b" (Const 30)),
-    EnrichedStmt (StmtMetadata (fromList ["c"])) (Let "c" (Var "a")),
-    EnrichedStmt (StmtMetadata (fromList ["b", "c"])) (Let "b" (Const 10)),
-    EnrichedStmt (StmtMetadata (fromList [])) (Let "b" (BinOp Add (Var "b") (Var "c")))
+    EnrichedStmt (StmtMetadata (fromList ["a"])) (Let "c" (Var "a")),
+    EnrichedStmt (StmtMetadata (fromList ["c"])) (Let "b" (Const 10)),
+    EnrichedStmt (StmtMetadata (fromList ["b", "c"])) (Let "b" (BinOp Add (Var "b") (Var "c")))
+  ]
+
+ex2 :: [Stmt]
+ex2 =
+  [ Let "a" (Const 5), --  {}
+    Let "b" (Var "a"), -- {a}
+    Let "c" (Var "a"), -- {a}
+    Let "d" (Const 10), -- {a}
+    Return (Var "a") -- {a}
+  ]
+
+livenessEx2 :: [Set T.Text]
+livenessEx2 =
+  [ fromList [],
+    fromList ["a"],
+    fromList ["a"],
+    fromList ["a"],
+    fromList ["a"]
   ]
 
 unitTests :: TestTree
@@ -35,7 +55,10 @@ unitTests =
     [ testCase "livness doesn't modify order of stmts" $ do
         let enrichedStmts = buildLiveness ex1
         assertEqual "" (fmap stmt enrichedEx1) (fmap stmt enrichedStmts),
-      testCase "liveness for ex1" $ do
+      testCase "liveness_ex1" $ do
         let enrichedStmts = buildLiveness ex1
-        assertEqual "" (fmap (liveness . stmtMetadata) enrichedEx1) (fmap (liveness . stmtMetadata) enrichedStmts)
+        assertEqual "" (fmap (liveness . stmtMetadata) enrichedEx1) (fmap (liveness . stmtMetadata) enrichedStmts),
+      testCase "liveness_ex2" $ do
+        let enrichedStmts = buildLiveness ex2
+        assertEqual "" livenessEx2 (fmap (liveness . stmtMetadata) enrichedStmts)
     ]
