8c7a7267c6fc452386c91d6e2203180260f16bd9
Gabriel Torello <gabriel128@gmail.com>
2023-11-27 20:20:59 +1100

    Fix resolver and tests

--- src/GeneralDS/Graph.hs
+++ src/GeneralDS/Graph.hs
@@ -3,25 +3,30 @@
 
 module GeneralDS.Graph where
 
-import Data.Either.Combinators (maybeToRight)
-import Data.List (foldl')
-import Data.Map (fromList)
-import qualified Data.Map as Map
-import Data.Map.Strict (Map, mapWithKey)
-import Data.Maybe (fromMaybe)
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Text (Text)
-import qualified Data.Text as T
-import GeneralDS.Queue (Queue)
-import qualified GeneralDS.Queue as Queue
-import GeneralDS.Stack (Stack)
-import qualified GeneralDS.Stack as Stack
+import           Data.Either.Combinators        ( maybeToRight )
+import           Data.List                      ( foldl' )
+import           Data.Map                       ( fromList )
+import qualified Data.Map                      as Map
+import           Data.Map.Strict                ( Map
+                                                , mapWithKey
+                                                )
+import           Data.Maybe                     ( fromMaybe )
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as Set
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as T
+import           GeneralDS.Queue                ( Queue )
+import qualified GeneralDS.Queue               as Queue
+import           GeneralDS.Stack                ( Stack )
+import qualified GeneralDS.Stack               as Stack
 
 newtype Graph a = Graph {graphNodes :: Map a (Node a)}
   deriving (Show, Eq, Ord)
 
-data Node a = Node {nodeVal :: !a, nodeEdges :: Set a}
+data Node a = Node
+  { nodeVal   :: !a
+  , nodeEdges :: Set a
+  }
   deriving (Show, Eq, Ord)
 
 newGraph :: Graph a
@@ -29,10 +34,10 @@ newGraph = Graph Map.empty
 
 isValidGraph :: Ord a => Graph a -> Bool
 isValidGraph (Graph graphNodes) =
-  let nodes = Map.elems graphNodes
-      edges = foldr Set.union Set.empty . fmap nodeEdges $ nodes
+  let nodes    = Map.elems graphNodes
+      edges    = foldr Set.union Set.empty . fmap nodeEdges $ nodes
       nodeVals = foldr Set.insert Set.empty . fmap nodeVal $ nodes
-   in edges == nodeVals
+  in  edges == nodeVals
 
 newNode :: Ord a => a -> [a] -> Node a
 newNode a edges = Node a (Set.fromList edges)
@@ -42,16 +47,28 @@ defaultNode a = Node a Set.empty
 
 insertEdge :: Ord a => (a, a) -> Graph a -> Graph a
 insertEdge (nodeIdA, nodeIdB) (Graph nodes) =
-  let insertFn nodeId createdNode oldNode = Node nodeId (nodeEdges createdNode `Set.union` nodeEdges oldNode)
-      nodes' = Map.insertWith (insertFn nodeIdA) nodeIdA (newNode nodeIdA [nodeIdB]) nodes
-      nodes'' = Map.insertWith (insertFn nodeIdB) nodeIdB (newNode nodeIdB [nodeIdA]) nodes'
-   in Graph nodes''
+  let
+    insertFn nodeId createdNode oldNode =
+      Node nodeId (nodeEdges createdNode `Set.union` nodeEdges oldNode)
+    nodes' = Map.insertWith (insertFn nodeIdA)
+                            nodeIdA
+                            (newNode nodeIdA [nodeIdB])
+                            nodes
+    nodes'' = Map.insertWith (insertFn nodeIdB)
+                             nodeIdB
+                             (newNode nodeIdB [nodeIdA])
+                             nodes'
+  in
+    Graph nodes''
 
 insertNode :: (Eq a, Ord a) => Node a -> Graph a -> Graph a
 insertNode node@(Node a edges) (Graph nodes) =
-  let insertEdgeToNode edge (Node val nodeEdges) = Node val (Set.insert edge nodeEdges)
-      oldNodes = mapWithKey (\k node' -> if k `elem` edges then insertEdgeToNode a node' else node') nodes
-   in Graph $ Map.insert a node oldNodes
+  let insertEdgeToNode edge (Node val nodeEdges) =
+        Node val (Set.insert edge nodeEdges)
+      oldNodes = mapWithKey
+        (\k node' -> if k `elem` edges then insertEdgeToNode a node' else node')
+        nodes
+  in  Graph $ Map.insert a node oldNodes
 
 {-
   a --- b -- d
@@ -76,11 +93,11 @@ insertNode node@(Node a edges) (Graph nodes) =
 -- Graph {nodes = fromList [(0,Node {nodeVal = 1, nodeEdges = fromList [0]})]}
 instance (Ord a) => Semigroup (Graph a) where
   (Graph lnodes) <> (Graph rnodes) = Graph $ foldr reducer lnodes rnodes
-    where
-      reducer rnode@(Node nodeId edges) acc =
-        let lnode' = fromMaybe rnode $ Map.lookup nodeId lnodes
-            newEdges = edges `Set.union` nodeEdges lnode'
-         in Map.insert nodeId (Node nodeId newEdges) acc
+   where
+    reducer rnode@(Node nodeId edges) acc =
+      let lnode'   = fromMaybe rnode $ Map.lookup nodeId lnodes
+          newEdges = edges `Set.union` nodeEdges lnode'
+      in  Map.insert nodeId (Node nodeId newEdges) acc
 
 instance (Ord a) => Monoid (Graph a) where
   mempty = newGraph
@@ -96,69 +113,111 @@ graphEx =
       nodeB = Node "b" (Set.fromList ["a", "d"])
       nodeC = Node "c" (Set.fromList ["a"])
       nodeD = Node "d" (Set.fromList ["b"])
-   in Graph (fromList [("a", nodeA), ("b", nodeB), ("c", nodeC), ("d", nodeD)])
+  in  Graph (fromList [("a", nodeA), ("b", nodeB), ("c", nodeC), ("d", nodeD)])
 
 -- | Returns a list of connected nodes in DFS order
-dfs :: forall a. (Show a, Ord a) => a -> Graph a -> Either Text [a]
-dfs source (Graph nodes) = reverse . snd <$> go (Map.lookup source nodes) Set.empty []
-  where
-    go :: Maybe (Node a) -> Set a -> [a] -> Either Text (Set a, [a])
-    go Nothing _ _ = Left "Source not found"
-    go (Just (Node nodeId edges)) visited result
-      | nodeId `Set.member` visited = Right (visited, result)
-      | otherwise = do
-        let visited' = Set.insert nodeId visited
-        foldl' reducer (Right (visited', nodeId : result)) edges
+dfs :: forall a . (Show a, Ord a) => a -> Graph a -> Either Text [a]
+dfs source (Graph nodes) =
+  reverse . snd <$> go (Map.lookup source nodes) Set.empty []
+ where
+  go :: Maybe (Node a) -> Set a -> [a] -> Either Text (Set a, [a])
+  go Nothing _ _ = Left "Source not found"
+  go (Just (Node nodeId edges)) visited result
+    | nodeId `Set.member` visited = Right (visited, result)
+    | otherwise = do
+      let visited' = Set.insert nodeId visited
+      foldl' reducer (Right (visited', nodeId : result)) edges
 
-    reducer :: Either Text (Set a, [a]) -> a -> Either Text (Set a, [a])
-    reducer visited nodeId = do
-      node <- maybeToRight (T.pack $ "Node " <> show nodeId <> " not found") (Map.lookup nodeId nodes)
-      (visited', result) <- visited
-      go (Just node) visited' result
+  reducer :: Either Text (Set a, [a]) -> a -> Either Text (Set a, [a])
+  reducer visited nodeId = do
+    node <- maybeToRight (T.pack $ "Node " <> show nodeId <> " not found")
+                         (Map.lookup nodeId nodes)
+    (visited', result) <- visited
+    go (Just node) visited' result
 
-bfs :: forall a. Ord a => a -> Graph a -> Either Text [(Word, a)]
+bfs :: forall a . Ord a => a -> Graph a -> Either Text [(Word, a)]
 bfs source (Graph nodes) = doBfs (Map.lookup source nodes)
-  where
-    doBfs Nothing = Left "Source not found"
-    doBfs (Just (Node nodeId _)) = go Set.empty (Queue.enqueue nodeId Queue.new) 0
+ where
+  doBfs Nothing = Left "Source not found"
+  doBfs (Just (Node nodeId _)) =
+    go Set.empty (Queue.enqueue nodeId Queue.new) 0
 
-    go :: Set a -> Queue a -> Word -> Either Text [(Word, a)]
-    go visited queue distance
-      | Queue.len queue == 0 = Right []
-      | otherwise = do
-        let currentNodeIds = Queue.toList queue
-        let visited' = foldr Set.insert visited currentNodeIds
-        currentNodes <- maybeToRight "Node not found" (mapM (`Map.lookup` nodes) currentNodeIds)
-        let nodeIdsToEqueue = concatMap (Set.toList . flip Set.difference visited' . nodeEdges) currentNodes
-        let nextQueue = foldl' (flip Queue.enqueue) Queue.new nodeIdsToEqueue
-        let resultWithDistance = fmap (distance,) currentNodeIds
-        (++) <$> Right resultWithDistance <*> go visited' nextQueue (distance + 1)
+  go :: Set a -> Queue a -> Word -> Either Text [(Word, a)]
+  go visited queue distance
+    | Queue.len queue == 0 = Right []
+    | otherwise = do
+      let currentNodeIds = Queue.toList queue
+      let visited'       = foldr Set.insert visited currentNodeIds
+      currentNodes <- maybeToRight "Node not found"
+                                   (mapM (`Map.lookup` nodes) currentNodeIds)
+      let nodeIdsToEqueue = concatMap
+            (Set.toList . flip Set.difference visited' . nodeEdges)
+            currentNodes
+      let nextQueue = foldl' (flip Queue.enqueue) Queue.new nodeIdsToEqueue
+      let resultWithDistance = fmap (distance, ) currentNodeIds
+      (++) <$> Right resultWithDistance <*> go visited' nextQueue (distance + 1)
 
 -- | Returns the node given a node val/id using DFS
-dfsNode :: forall a. Ord a => a -> Graph a -> Maybe (Node a)
+dfsNode :: forall a . Ord a => a -> Graph a -> Maybe (Node a)
 dfsNode val (Graph nodes) = go (Map.elems nodes)
-  where
-    go [] = Nothing
-    go (node : _) = recurr Set.empty (Stack.push (nodeVal node) Stack.new)
+ where
+  go []         = Nothing
+  go (node : _) = recurr Set.empty (Stack.push (nodeVal node) Stack.new)
 
-    recurr :: Set a -> Stack a -> Maybe (Node a)
-    recurr visitedNodeIds nextNodeIds = do
-      (node, newNextNodeIds) <- popNode nextNodeIds
+  recurr :: Set a -> Stack a -> Maybe (Node a)
+  recurr visitedNodeIds nextNodeIds = do
+    (node, newNextNodeIds) <- popNode nextNodeIds
 
-      let nodeId = nodeVal node
-      let edges = nodeEdges node
+    let nodeId = nodeVal node
+    let edges  = nodeEdges node
 
-      case (nodeVal node == val, Set.member nodeId visitedNodeIds) of
-        (True, _) -> Just node
-        (False, True) -> recurr visitedNodeIds newNextNodeIds
-        (False, False) ->
-          let nextSet = Set.insert nodeId visitedNodeIds
-              nextStack = foldr Stack.push newNextNodeIds edges
-           in recurr nextSet nextStack
+    case (nodeVal node == val, Set.member nodeId visitedNodeIds) of
+      (True , _   ) -> Just node
+      (False, True) -> recurr visitedNodeIds newNextNodeIds
+      (False, False) ->
+        let nextSet   = Set.insert nodeId visitedNodeIds
+            nextStack = foldr Stack.push newNextNodeIds edges
+        in  recurr nextSet nextStack
 
-    popNode :: Stack a -> Maybe (Node a, Stack a)
-    popNode nextNodeIds = do
-      let (maybeNodeId, newNextNodeIds) = Stack.pop nextNodeIds
-      nodeId <- maybeNodeId
-      node <- Map.lookup nodeId nodes
-      pure (node, newNextNodeIds)
+  popNode :: Stack a -> Maybe (Node a, Stack a)
+  popNode nextNodeIds = do
+    let (maybeNodeId, newNextNodeIds) = Stack.pop nextNodeIds
+    nodeId <- maybeNodeId
+    node   <- Map.lookup nodeId nodes
+    pure (node, newNextNodeIds)
+
+-- | Graph coloring
+-- W <- vertices(G)
+-- while W /= {} do
+--   pick a vertex u from W with maximal saturation
+--   find the lowest color c not in { color[v] | v in adjacent(u) }.
+--   color[u] <- c
+--   W <- W - {u}
+type GraphColor = Int
+
+type SaturationMap a = Map a (Set GraphColor)
+
+newSaturationMap :: Set (Node a) -> SaturationMap a
+newSaturationMap nodes = Map.empty
+
+maxSaturated :: SaturationMap a -> a
+maxSaturated = undefined
+
+maxSaturatedNotVisited :: SaturationMap a -> Set (Node a) -> Node a
+maxSaturatedNotVisited = undefined
+
+colorsNotInAdj :: Set GraphColor -> Set a -> Set GraphColor
+colorsNotInAdj = undefined
+
+-- | Given a graph and a set of graph colours, it maps the 
+colorGraph
+  :: forall a . Ord a => Graph a -> Set GraphColor -> Map a GraphColor
+colorGraph graph colors =
+  let w = Set.fromList (Map.elems $ graphNodes graph)
+  in  while w (newSaturationMap w) Set.empty Map.empty
+  where while = undefined
+    -- while [] _saturationMap _visited res = res
+    -- while w saturationMap visited colorMapping =
+    --   let u = maxSaturatedNotVisited saturationMap visited
+    --       c = min (colorsNotInAdj colors (nodeEdges u))
+    --    in while (Set.delete u w) saturationMap (Set.insert u visited) (Map.insert (nodeVal u) c colorMapping)
--- test/RegisterAllocTests.hs
+++ test/RegisterAllocTests.hs
@@ -1,12 +1,18 @@
-module RegisterAllocTests (test_reg_alloc) where
+module RegisterAllocTests
+  ( test_reg_alloc
+  ) where
 
-import Ast.Ast
-import Data.Set
-import qualified Data.Text as T
-import Passes.RegisterAlloc
-import Passes.RegisterAlloc (stmt)
-import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (assertBool, assertEqual, testCase)
+import           Ast.Ast
+import           Data.Set
+import qualified Data.Text                     as T
+import           Passes.RegisterAlloc
+import           Test.Tasty                     ( TestTree
+                                                , testGroup
+                                                )
+import           Test.Tasty.HUnit               ( assertBool
+                                                , assertEqual
+                                                , testCase
+                                                )
 
 test_reg_alloc :: TestTree
 test_reg_alloc = testGroup "Tests" [unitTests]
@@ -14,53 +20,62 @@ test_reg_alloc = testGroup "Tests" [unitTests]
 -- We save the livness after
 ex1 :: [Stmt]
 ex1 =
-  [ Let "a" (Const 5), -- {a}
-    Let "b" (Const 30), -- {a}
-    Let "c" (Var "a"), -- {c}
-    Let "b" (Const 10), -- {b, c}
+  [ Let "a" (Const 5)
+  , -- {a}
+    Let "b" (Const 30)
+  , -- {a}
+    Let "c" (Var "a")
+  , -- {c}
+    Let "b" (Const 10)
+  , -- {b, c}
     Let "b" (BinOp Add (Var "b") (Var "c")) -- {}
   ]
 
 enrichedEx1 :: [EnrichedStmt]
 enrichedEx1 =
-  [ EnrichedStmt (fromList ["a"]) (fromList ["a"]) (Let "a" (Const 5)),
-    EnrichedStmt (fromList ["a"]) (fromList ["b"]) (Let "b" (Const 30)),
-    EnrichedStmt (fromList ["c"]) (fromList ["c"]) (Let "c" (Var "a")),
-    EnrichedStmt (fromList ["b", "c"]) empty (Let "b" (Const 10)),
-    EnrichedStmt (fromList [""]) (fromList ["b"]) (Let "b" (BinOp Add (Var "b") (Var "c")))
+  [ EnrichedStmt (fromList ["a"])      (fromList ["a"]) (Let "a" (Const 5))
+  , EnrichedStmt (fromList ["a"])      (fromList ["b"]) (Let "b" (Const 30))
+  , EnrichedStmt (fromList ["c"])      (fromList ["c"]) (Let "c" (Var "a"))
+  , EnrichedStmt (fromList ["b", "c"]) (fromList ["b"]) (Let "b" (Const 10))
+  , EnrichedStmt (fromList [])
+                 (fromList ["b"])
+                 (Let "b" (BinOp Add (Var "b") (Var "c")))
   ]
 
 ex2 :: [Stmt]
 ex2 =
-  [ Let "a" (Const 5), --  {a}
-    Let "b" (Var "a"), -- {a}
-    Let "c" (Var "a"), -- {a}
-    Let "d" (Const 10), -- {a}
+  [ Let "a" (Const 5)
+  , --  {a}
+    Let "b" (Var "a")
+  , -- {a}
+    Let "c" (Var "a")
+  , -- {a}
+    Let "d" (Const 10)
+  , -- {a}
     Return (Var "a") -- {}
   ]
 
 livenessAfterEx2 :: [(Set T.Text, Set T.Text)]
 livenessAfterEx2 =
-  [ (fromList ["a"], fromList ["a"]),
-    (fromList ["a"], fromList ["b"]),
-    (fromList ["a"], fromList ["c"]),
-    (fromList ["a"], fromList ["d"]),
-    (fromList [], empty)
+  [ (fromList ["a"], fromList ["a"])
+  , (fromList ["a"], fromList ["b"])
+  , (fromList ["a"], fromList ["c"])
+  , (fromList ["a"], fromList ["d"])
+  , (fromList []   , empty)
   ]
 
 unitTests :: TestTree
-unitTests =
-  testGroup
-    "Register Allocation tests"
-    [ testCase "livness doesn't modify order of stmts" $ do
-        let enrichedStmts = buildLiveness ex1
-        assertEqual "" (fmap stmt enrichedEx1) (fmap stmt enrichedStmts),
-      testCase "liveness_ex1" $ do
-        let enrichedStmts = buildLiveness ex1
-        assertEqual "" enrichedEx1 enrichedStmts,
-      testCase "liveness_ex2" $ do
-        let enrichedStmts = buildLiveness ex2
-            liveness = fmap livenessAfter enrichedStmts
-            writeSet = fmap stmtWriteSet enrichedStmts
-        assertEqual "" livenessAfterEx2 (zip liveness writeSet)
-    ]
+unitTests = testGroup
+  "Register Allocation tests"
+  [ testCase "livness doesn't modify order of stmts" $ do
+    let enrichedStmts = buildLiveness ex1
+    assertEqual "" (fmap stmt enrichedEx1) (fmap stmt enrichedStmts)
+  , testCase "liveness_ex1" $ do
+    let enrichedStmts = buildLiveness ex1
+    assertEqual "" enrichedEx1 enrichedStmts
+  , testCase "liveness_ex2" $ do
+    let enrichedStmts = buildLiveness ex2
+        liveness      = fmap livenessAfter enrichedStmts
+        writeSet      = fmap stmtWriteSet enrichedStmts
+    assertEqual "" livenessAfterEx2 (zip liveness writeSet)
+  ]
